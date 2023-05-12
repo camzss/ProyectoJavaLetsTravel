5a0b571decefd4b3272b7e67fcada4919ed5eea0
mauricubas <bogadomauricio2@gmail.com>
2023-05-11 21:14:13 -0400

    update de camila

--- src/main/java/com/example/Agente/controller/AgenteController.java
+++ src/main/java/com/example/Agente/controller/AgenteController.java
@@ -1,13 +1,28 @@
 
 package com.example.Agente.controller;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.Agente.form.AgenteForm;
 
 /**
  *
  * @author PC
  */
-@Component
+@Controller
 public class AgenteController {
-    
+    @RequestMapping(value = "/agente", method = RequestMethod.GET)
+    public String getAgenteForm() {
+        return "agente";
+    }
+
+    @RequestMapping(value = "/agente", method = RequestMethod.POST)
+    public String agente(@ModelAttribute(name = "agenteForm") AgenteForm agenteForm, Model model) {
+        return "agente";
+    }
+
 }
---
+++ src/src/main/java/com/example/Destino/controller/DestinoController.java
@@ -0,0 +1,14 @@
+
+package com.example.Destino.controller;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author PC
+ */
+@Component
+public class DestinoController {
+    private int id_destino;
+    
+}
